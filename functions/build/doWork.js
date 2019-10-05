7a13b3f068be18ab077b78d6136bbffeb3869c83
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-10-04 22:11:29 -0500

    revert

--- functions/build/doWork.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
-
-const fetch = require("node-fetch");
-exports.handler = async function(event, context) {
-  return await fetch("https://api.chucknorris.io/jokes/random");
-};
